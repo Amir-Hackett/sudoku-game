7a1e2595b1d94de3c313df49edcec861bdeeaf3f
Amir <amir.hackett@gmail.com>
2023-09-27 11:12:19 -0400

    fixed error for numbers not working

--- sudoku.js
+++ sudoku.js
@@ -32,41 +32,41 @@ window.onload = function() {
 }
 
 function setGame() {
-    // Digits 1 - 9//
-    for (let i = 1; i<=9; i++) {
+    // Digits 1-9
+    for (let i = 1; i <= 9; i++) {
         //<div id="1" class="number">1</div>
         let number = document.createElement("div");
-        number.id = 1;
+        number.id = i
         number.innerText = i;
-        number.addEventListener("click", selectNumber)
+        number.addEventListener("click", selectNumber);
         number.classList.add("number");
         document.getElementById("digits").appendChild(number);
     }
 
-    //Board 9x9//
+    // Board 9x9
     for (let r = 0; r < 9; r++) {
-        for (let c=0; c < 9; c++) {
+        for (let c = 0; c < 9; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             if (board[r][c] != "-") {
                 tile.innerText = board[r][c];
-                tile.classList.add("tile-start")
+                tile.classList.add("tile-start");
             }
             if (r == 2 || r == 5) {
                 tile.classList.add("horizontal-line");
             }
-            if (c == 2 || c === 5) {
+            if (c == 2 || c == 5) {
                 tile.classList.add("vertical-line");
             }
-            tile.addEventListener("click", selectTile)
+            tile.addEventListener("click", selectTile);
             tile.classList.add("tile");
             document.getElementById("board").append(tile);
         }
     }
 }
 
-function selectNumber() {
-    if(numSelected != null) {
+function selectNumber(){
+    if (numSelected != null) {
         numSelected.classList.remove("number-selected");
     }
     numSelected = this;
@@ -74,10 +74,22 @@ function selectNumber() {
 }
 
 function selectTile() {
-    if(numSelected){
+    if (numSelected) {
         if (this.innerText != "") {
             return;
         }
-        this.innerText = numSelected.id;
+
+        // "0-0" "0-1" .. "3-1"
+        let coords = this.id.split("-"); //["0", "0"]
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if (solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        }
+        else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
+        }
     }
 }
