085257a81ab63b3bfa97cd58ad407b71cb05459a
Amir <amir.hackett@gmail.com>
2023-09-27 10:25:51 -0400

    updated board with tiles

--- sudoku.js
+++ sudoku.js
@@ -41,4 +41,14 @@ function setGame() {
         number.classList.add("number");
         document.getElementById("digits").appendChild(number);
     }
+
+    //Board 9x9//
+    for (let r = 0; r < 9; r++) {
+        for (let c=0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "_" + c.toString();
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
 }
